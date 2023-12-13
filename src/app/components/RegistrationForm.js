5b0819505648743e139d202dd460fc64764e0ca6
Lee Cloete <lee-cloete@users.noreply.github.com>
2023-12-13 22:32:50 +0200

    asdsada
    asdasdasda

--- src/app/components/RegistrationForm.js
+++ src/app/components/RegistrationForm.js
@@ -19,7 +19,7 @@ const RegistrationForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('/api/register', {
+      const response = await fetch('/pages/api/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
